d80ab74b9e7a1971d3a0738a0cc4343d973628b1
James Dolan <jamesdolan1989@gmail.com>
2021-03-13 17:21:06 -0500

    React router now redirecting to home

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import "./App.css";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import React, { useEffect } from "react";
 import Login from "../src/components/Login";
+import Home from "./components/Home";
 
 const clientId = "x6fjleftolmy44txegskqtkq1t358y";
-const redirectUri = "http://localhost";
+const redirectUri = "http://localhost:3000/home";
 const responseType = "token";
 // const scopeList = ["chat:read", "chat:edit"].join().replace(",", "+");
 const scopeList = ["chat:read"];
@@ -15,16 +17,23 @@ const link = `https://id.twitch.tv/oauth2/authorize?client_id=${clientId}&redire
 //   ).then((response) => console.log("test"));
 // };
 
-function App(): JSX.Element {
+export default function App(): JSX.Element {
   return (
-    <div>
-      <a href={link} onClick={() => console.log(link)}>
-        3head
-      </a>
-    </div>
+    <Router>
+      <a href={link}>test</a>
+      <Switch>
+        <Route path='/home'>
+          <Home />
+        </Route>
+        <Route path='/login'>
+          <Login />
+        </Route>
+      </Switch>
+    </Router>
+    // <div>
+    //   <a href={link} onClick={() => console.log(link)}>
+    //     3head
+    //   </a>
+    // </div>
   );
-
-  // return <Login></Login>;
 }
-
-export default App;
---
+++ src/components/Home.tsx
@@ -0,0 +1,11 @@
+import React from 'react'; 
+
+interface HomeProps{
+
+}
+
+export default function Home(props: HomeProps) {
+    return (
+        <div>wat</div>
+    )
+}
